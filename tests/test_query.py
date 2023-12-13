6f2fb233564fbc89fe079c11f01e9216060db451
Justin Xu <justin13601@hotmail.com>
2023-12-13 01:53:02 +0000

    Some tests

--- tests/test_query.py
+++ tests/test_query.py
@@ -27,8 +27,54 @@ class TestQueryFunctions(unittest.TestCase):
             schema={
                 "subject_id": pl.UInt32,
                 "timestamp": pl.Datetime,
-            },
+            }
         )
+
+        test_1_predicates_df = pl.DataFrame(
+            {
+                'subject_id': [1, 1, 1],
+                'timestamp': ['12/1/1900 12:00', '12/1/1900 13:00', '12/1/1900 14:00'],
+                'event_type': ['ADMISSION', 'LAB', 'DISCHARGE'],
+                'dx': ['', '', ''],
+                'lab_test': ['', 'SpO2', ''],
+                'lab_value': ['', '99', ''],
+                'is_admission': [1, 0, 0],
+                'is_lab': [0, 1, 0],
+                'is_discharge': [0, 0, 1],
+                'is_any': [1, 1, 1],
+            }
+        ).with_columns(pl.col('timestamp').str.strptime(pl.Datetime, format='%m/%d/%Y %H:%M').cast(pl.Datetime))
+        test_1_predicate_cols = [col for col in test_1_predicates_df.columns if col.startswith('is_')]
+        test_1_endpoint_expr = (False, timedelta(hours=2), True, None) 
+        test_1_anchor_to_subtree_root_by_subtree_anchor = pl.DataFrame(
+            {
+                'subject_id': [1],
+                'timestamp': ['12/1/1900 12:00'],
+                'is_admission': [1],
+                'is_lab': [0],
+                'is_discharge': [0],
+                'is_any': [1],
+            }
+        ).with_columns(pl.col('timestamp').str.strptime(pl.Datetime, format='%m/%d/%Y %H:%M').cast(pl.Datetime))
+        test_1_result = pl.DataFrame(
+            {
+                'subject_id': [1],
+                'timestamp': ['12/1/1900 12:00'],
+                'timestamp_at_anchor': ['12/1/1900 12:00'],
+                'is_admission': [0],
+                'is_lab': [1],
+                'is_discharge': [1],
+                'is_any': [2],
+                'is_admission_summary': [1],
+                'is_lab_summary': [0],
+                'is_discharge_summary': [0],
+                'is_any_summary': [1],
+            }
+        ).with_columns(
+            pl.col('timestamp').str.strptime(pl.Datetime, format='%m/%d/%Y %H:%M').cast(pl.Datetime), 
+            pl.col('timestamp_at_anchor').str.strptime(pl.Datetime, format='%m/%d/%Y %H:%M').cast(pl.Datetime)
+        )
+
         cases = [
             {
                 "msg": "When passed an empty dataframe should return an empty dataframe",
@@ -37,6 +83,14 @@ class TestQueryFunctions(unittest.TestCase):
                 "endpoint_expr": (True, timedelta(seconds=10), False, timedelta(seconds=20)),
                 "anchor_to_subtree_root_by_subtree_anchor": empty_dataframe,
                 "want": empty_dataframe.with_columns(pl.col("timestamp").alias("timestamp_at_anchor")),
+            },
+            {
+                "msg": "Testing summarize_event_bound_window with simple dataframe and None as offset, should be equal",
+                "predicates_df": test_1_predicates_df,
+                "predicate_cols": test_1_predicate_cols,
+                "endpoint_expr": test_1_endpoint_expr,
+                "anchor_to_subtree_root_by_subtree_anchor": test_1_anchor_to_subtree_root_by_subtree_anchor,
+                "want": test_1_result,
             },
         ]
 
@@ -50,12 +104,14 @@ class TestQueryFunctions(unittest.TestCase):
         raise NotImplementedError
 
     def test_summarize_window(self):
+        raise NotImplementedError
+        print('---Temporally-Bound Case---')
         with patch("esgpt_task_querying.query.summarize_temporal_window") as mock_summarize_temporal_window:
             with patch(
                 "esgpt_task_querying.query.summarize_event_bound_window"
             ) as mock_summarize_event_bound_window:
                 summarize_window(
-                    child=None,  # TODO(justin): Fix
+                    child=None,  # TODO(justin)
                     anchor_to_subtree_root_by_subtree_anchor=pl.DataFrame(
                         {"subject_id": [], "timestamp": []}
                     ),
@@ -64,6 +120,23 @@ class TestQueryFunctions(unittest.TestCase):
                 )
                 mock_summarize_temporal_window.assert_called_once()
                 mock_summarize_event_bound_window.assert_not_called()
+        
+        print('---Event-Bound Case---')
+        with patch("esgpt_task_querying.query.summarize_temporal_window") as mock_summarize_temporal_window:
+            with patch(
+                "esgpt_task_querying.query.summarize_event_bound_window"
+            ) as mock_summarize_event_bound_window:
+                summarize_window(
+                    child=None,  # TODO(justin)
+                    anchor_to_subtree_root_by_subtree_anchor=pl.DataFrame(
+                        {"subject_id": [], "timestamp": []}
+                    ),
+                    predicates_df=pl.DataFrame({"subject_id": [], "timestamp": []}),
+                    predicate_cols=[],
+                )
+                mock_summarize_temporal_window.assert_not_called()
+                mock_summarize_event_bound_window.assert_called_once()
+
 
     def test_query_subtree(self):
         raise NotImplementedError
