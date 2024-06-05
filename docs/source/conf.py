2d7e4564bf046e2b4d03e8d155684f4d68d4a010
Justin Xu <justin13601@hotmail.com>
2024-06-05 02:16:01 +0100

    Logo changes, fix file names, update logs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -262,12 +262,12 @@ html_short_title = "ACES Documentation"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "query-512.png"
+html_logo = "assets/aces_logo.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "query-16.ico"
+html_favicon = "assets/aces_logo.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
