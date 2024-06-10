36362939d95edc1239c6f736246ea97d7677f618
Justin Xu <justin13601@hotmail.com>
2024-06-10 19:17:52 +0100

    Remove decoding

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -327,7 +327,7 @@ latex_elements = {  # type: ignore
     # The font size ("10pt", "11pt" or "12pt").
     "pointsize": "10pt",
     # Additional stuff for the LaTeX preamble.
-    "preamble": "".join((r"\DeclareUnicodeCharacter{2501}{━}",)),
+    # "preamble": "".join((r"\DeclareUnicodeCharacter{2501}{━}",)),
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
