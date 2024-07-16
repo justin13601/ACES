bb43ef4f8bcb2c1165245ab1775dcb2ca34187fd
Justin Xu <justin13601@hotmail.com>
2024-07-17 00:50:51 +0100

    Adds a version number

--- src/aces/__init__.py
+++ src/aces/__init__.py
@@ -2,3 +2,11 @@
 
 It contains the main functions and classes needed to extract cohorts.
 """
+
+from importlib.metadata import PackageNotFoundError, version
+
+__package_name__ = "MEDS_polars_functions"
+try:
+    __version__ = version(__package_name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
